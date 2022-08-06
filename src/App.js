cafe7786fc68456361aaa04154de53b2f6291f59
HannaVid <hannaviden@gmail.com>
2022-08-06 13:53:57 +0300

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      React Weather App
+      <h1>React Weather App</h1>
+      <footer>
+        This project was coded by Hanna Vidieniova and is {" "}
+        <a href="https://github.com/HannaVid/react-weather-app" title="See open-sourced code of the Project on Github" rel="noreferrer" target="_blank">open-sourced code on GitHub</a> 
+      </footer>
     </div>
   );
 };

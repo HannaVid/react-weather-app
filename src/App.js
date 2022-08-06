7cfb2be561f20b51a1abf7d54c4195af830e9dcd
HannaVid <hannaviden@gmail.com>
2022-08-06 16:40:45 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>React Weather App</h1>
+      
       <footer>
         This project was coded by Hanna Vidieniova and is {" "}
         <a href="https://github.com/HannaVid/react-weather-app" title="See open-sourced code of the Project on Github" rel="noreferrer" target="_blank">open-sourced code on GitHub</a> 
       </footer>
+      </div>
     </div>
   );
 };
